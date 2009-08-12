5dcc43f9d3f9c4ccc988dfaed20642f98927340f
Sanket Agarwal <snktagarwal@gmail.com>
2009-08-12 21:24:36 +0530

    Adding license to activity-logger

--- activity-logger.py
+++ activity-logger.py
@@ -1,3 +1,26 @@
+#The MIT License
+
+#Copyright (c) 2009 Sanket Agarwal( mailto: snktagarwal@gmail.com )
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+
 """ This script is meant to log the number of logged on users at any given point of time. So this is what I will do.
 	Whenever a person logs on or logs off Pidgin emits signals notifying that something has happened. I would take the
 	initial person count and then add/del the users according to the emmited signal!"""
